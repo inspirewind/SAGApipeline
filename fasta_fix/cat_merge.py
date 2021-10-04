4e033c90a574352ee6ce8f6544048a6819dfdc05
inspirewind <Lr2001912@gmail.com>
2021-10-05 01:02:35 +0800

    Nat update 3

--- fasta_fix/cat_merge.py
+++ fasta_fix/cat_merge.py
@@ -1,8 +1,8 @@
 import os
 import sys
 
-if os.name != 'posix':
-    sys.exit(1)
+# if os.name != 'posix':
+#     sys.exit(1)
 
 class cater():
     def __init__(self, top) -> None:
@@ -16,7 +16,18 @@ class cater():
             for genome in self.genomes:
                 genome_path = os.path.join(self.top, genome)
                 os.system("echo 'pwd: %s'" % (genome_path))
+                os.system("cd %s && if [ -f rna.fna ]; then mv rna.fna rna.f_n_a.donotmerge; echo 'rna has masked!'; fi" % (genome_path))
                 os.system("cd %s && cat *.fna > merge.fna && echo '%s merged!'" % (genome_path, genome_path))
+        else:
+            print("OsError!")
+    
+    def remove(self):
+        if os.name == 'posix':
+            print("os.name is posix, can be performed")
+            for genome in self.genomes:
+                genome_path = os.path.join(self.top, genome)
+                os.system("echo 'pwd: %s'" % (genome_path))
+                os.system("cd %s && rm -f merge.fna && echo '%s removed!'" % (genome_path, genome_path))
         else:
             print("OsError!")
 
